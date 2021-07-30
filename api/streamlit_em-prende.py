93d6b6cb8f88b050cb0168d13a3fafaa2122cd91
mmendezs2021 <85966577+mmendezs2021@users.noreply.github.com>
2021-07-30 15:32:37 -0500

    Update streamlit_em-prende.py

--- api/streamlit_em-prende.py
+++ api/streamlit_em-prende.py
@@ -7,7 +7,7 @@ entradas = []
 
 c = st.beta_columns([2,2])
 #c[0].title("Em-prende!!")
-c[1].image(img,width = 220)
+c[1].image(img,width = 210)
 
 st.header("¿Quieres averiguar como les va a otros negocios en tu mercado?")
 
